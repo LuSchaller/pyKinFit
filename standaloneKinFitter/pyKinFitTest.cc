eb3e5b42eca2481ef8323d9904a1d15242bfb032
Lukas Schaller <luckyschaller@gmail.com>
2025-05-06 15:14:43 +0200

    ?

--- standaloneKinFitter/pyKinFitTest.cc
+++ standaloneKinFitter/pyKinFitTest.cc
@@ -7,5 +7,5 @@ namespace py = pybind11;
 PYBIND11_MODULE(pyKinFitTest, m) {
     m.doc() = "KinFitter module"; // optional module docstring
 
-    m.def("setBestCombi", &setBestCombi, "A function that returns the best combination of jets and their fitted values based on the chi2 value of the kinematic fit", py::arg("inputpt"), py::arg("inputeta"), py::arg("inputphi"), py::arg("inputM"));
+    m.def("setBestCombi", &setBestCombi, "A function that returns the best combination of jets and their fitted values based on the chi2 value of the kinematic fit", py::arg("inputpt"), py::arg("inputeta"), py::arg("inputphi"), py::arg("inputM"), py::return_value_policy::automatic);
 }
